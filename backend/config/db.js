9d1601eae0d9e2158d03765d9eea36af48b8be33
Aditya Meena <adityameena2233@gmail.com>
2026-05-14 15:55:16 +0530

    postgres and database setup done

---
+++ backend/config/db.js
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+import { text } from 'express';
+
+dotenv.config();
+
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
+});
+
+
+pool.on('connect', () => {
+  console.log('Connected to the postgres database');
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+export default {
+  query: (text, params) => pool.query(text, params),
+  pool
+}
